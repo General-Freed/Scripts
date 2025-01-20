5693f288e6fac3a8b908d63b5f71b838c2df2e9c
XStyhler <d.styhler@googlemail.com>
2025-01-20 15:58:19 +0100

    Update ROGAllyOptimizer.ps1
    PNPDevices Update

--- ROGAllyOptimizer.ps1
+++ ROGAllyOptimizer.ps1
@@ -1,36 +1,26 @@
 write-host "Disable Services"
-
 $services = @("BITS","BTAGService","bthserv","lfsvc","DiagTrack","HvHost","vmickvpexchange","vmicguestinterface","vmicshutdown","vmicheartbeat","vmicvmsession","vmicrdv","vmictimesync","vmicvss","PhoneSvc","Spooler","QWAVE","SysMain","WSearch")
-
 foreach($s in $services) {
-    #get-service $s | FT status,name,start*
     Set-Service $s -StartupType Disabled
 }
-
 #Auto: DiagTrack, Spooler, SysMain
 
 write-host "Deactivate Devices"
-
-$devices = @("Enumerator für virtuelle NDIS-Netzwerkadapter","Microsoft virtueller Datenträgerenumerator","Hochpräzisionsereigniszeitgeber","Redirector-Bus für Remotedesktop-Gerät")
-
+#$devices = @("Enumerator für virtuelle NDIS-Netzwerkadapter","Microsoft virtueller Datenträgerenumerator","Hochpräzisionsereigniszeitgeber","Redirector-Bus für Remotedesktop-Gerät")
+$devices = @("ROOT\NDISVIRTUALBUS\0000","ROOT\VDRVROOT\0000","ACPI\PNP0103\2&DABA3FF&0","ROOT\RDPBUS\0000")
 foreach($d in $devices) {
-
-    Get-PnpDevice | ? Friendly name -eq $d | Disable-PnpDevice -Confirm:$false
-
+    #Get-PnpDevice | ? Friendly name -eq $d | Disable-PnpDevice -Confirm:$false
+    Get-PnpDevice $d | ft InstanceID,Friendlyname
+    Get-PnpDevice -InstanceId $d | Disable-PnpDevice -Confirm:$false
 }
 
 write-host "Reg Keys"
-
 set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\" -Name "SystemResponsiveness" -Value 0
-
 set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" -Name "Priority" -Value 6
-
 set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile\Tasks\Games" -Name "GPU Priority" -Value 8
-
 set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\PriorityControl" -Name "Win32PrioritySeparation" -Value 1
 
 Write-Host "Swap File 32GB"
-
 $pagefile = Get-CimInstance -ClassName Win32_ComputerSystem
 $pagefile.AutomaticManagedPagefile = $false
 Set-CimInstance -InputObject $pagefile
