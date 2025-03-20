202c973348f67dd5ea0d55ef71b759f9963d50f6
XStyhler <d.styhler@googlemail.com>
2025-03-20 11:38:50 +0100

    Files into folders.ps1 erstellen

---
+++ Files into folders.ps1
@@ -0,0 +1,14 @@
+$Start = Read-Host "Anfangsdatum"
+$End = Read-Host "Enddatum"
+$Target = Read-Host "Verzeichnis"
+
+$StartDate = $Start.ToDate()
+$EndDate = $End.ToDate()
+
+$files = Get-ChildItem "C:\files\"
+
+for each($f in $files) {
+if ($f.CreationTime -ge $StartDate -and $f.CreationTime -le $EndDate) {
+$f.movefile($Target)
+}
+}
